a4a4d4a94c389b2261f8276dfcff8103b6bb0a1b
DESKTOP-0I8FFQ7\USER <byswiy99@naver.com>
2022-04-25 13:54:08 +0900

    기능 수행을 위한 service.java 추가

---
+++ 2022-04-25/src/main/java/service/MemberService.java
@@ -0,0 +1,14 @@
+package service;
+
+import dto.MemberDTO;
+
+public class MemberService {
+	
+	public void join(MemberDTO newMemberInfo) {
+		// 회원 가입 처리
+		
+		// DB 연결
+		// 쿼리 생성, 실행, ... , 해제
+		// DB 연결 해제
+	}
+}
