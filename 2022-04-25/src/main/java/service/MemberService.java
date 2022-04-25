9ea8031a89ab68caba54a827b2469681261bd407
DESKTOP-0I8FFQ7\USER <byswiy99@naver.com>
2022-04-25 14:15:44 +0900

    MemberService.java : 회원가입 처리를 DAO에서 대신 해주고 있음, 회원가입 처리 결과 반환

--- 2022-04-25/src/main/java/service/MemberService.java
+++ 2022-04-25/src/main/java/service/MemberService.java
@@ -1,14 +1,28 @@
 package service;
 
+import dao.MemberDAO;
 import dto.MemberDTO;
 
 public class MemberService {
 	
-	public void join(MemberDTO newMemberInfo) {
+	public int join(MemberDTO newMemberInfo) {
 		// 회원 가입 처리
+		MemberDAO dao = new MemberDAO();
 		
-		// DB 연결
-		// 쿼리 생성, 실행, ... , 해제
-		// DB 연결 해제
+		boolean result = dao.insertMemberInfo(newMemberInfo);
+		
+		// 회원 가입 처리 결과 반환
+		// 회원 가입 성공 : ~~ 반환 / 상태코드 201 반환
+		// 회원 가입 실패
+		// 1. 이미 사용중인 아이디 : ~~ 반환 / 409 반환
+		// 결과가 성공 / 실패 두 가지 경우이기 때문에 boolean 타입으로 return result를 해줘도 된다
+		// 결과를 boolean이 아닌 상태코드를 전달해줄 수 있다
+		
+		if(result) {
+			return 200;
+		} else {
+			return 409;
+		}
+//		return result;
 	}
 }
